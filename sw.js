3f010c75e83ffd46017c9d26e74e2f3d054842d4
Anita C <huilong@gmail.com>
2021-01-07 14:21:34 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-62db2e7e62e3427ac08e.js"
+    "url": "webpack-runtime-523857e5419101870b29.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7712e319873bdfd6be63a7758f8ffb05"
+    "revision": "f3f4c468b8977d8b77643318901b3bf4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8ef872530b850e49bc88501c4940090e"
+    "revision": "b9ebf5e75156dc20f35a605a8e98bc62"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
